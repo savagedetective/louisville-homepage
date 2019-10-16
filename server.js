fbf3732e3aad70a8e2511e0dbbfb85bf440966c2
Ritchie <detectives.salvajes@protonmail.com>
2019-10-15 20:12:47 -0400

    server...serves test index.html

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static("app/public"));
-
-// // Routes
-// require("./app/routes/api-routes.js")(app);
-// require("./app/routes/html-routes.js")(app);
-
-app.get('/', function (req, res) {
-  console.log("Got a GET request for the homepage");
-  res.send('Hello GET');
-})
+//Routes
+//require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, function() {
   console.log("Goliath online: " + PORT);
