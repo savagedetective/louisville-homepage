340e4363f5b15767bab366e87556a3c95a170a63
Ritchie <detectives.salvajes@protonmail.com>
2019-10-17 19:34:13 -0400

    Adds future forecast route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,4 +21,24 @@ module.exports = function (app) {
                 });
         
     });
+
+    app.get("/api/forecast", function (req, res) {
+
+        console.log("API route hit");
+
+        const api = process.env.API_KEY;
+        const url = `https://api.openweathermap.org/data/2.5/forecast?zip=40204&appid=${api}`
+
+        axios.get(url)
+            .then(function(results) {
+
+                const data = results.data;
+                res.json(data);
+
+            })
+                .catch((err) => {
+                    console.log(err);
+                });
+        
+    });
 };
