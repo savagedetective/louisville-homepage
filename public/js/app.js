61f5155a1cfeb1c94b5b34036c19c6448fab7ff0
Ritchie <detectives.salvajes@protonmail.com>
2019-10-20 16:48:40 -0400

    data for each day populates

--- public/js/app.js
+++ public/js/app.js
@@ -36,8 +36,6 @@ $(document).ready(function () {
 
         $.get("/api/weather").then(function (data) {
 
-            console.log(data);
-
             const tempKelvin = data.main.temp;
             const tempDegrees = (tempKelvin * (9 / 5) - 459.67).toFixed(1);
             const description = data.weather[0]["description"];
@@ -75,13 +73,124 @@ $(document).ready(function () {
 
             $(".temp-holder").html(`<p>${tempDegrees}</p>`);
             $(".temp-holder").append(`<p>${description}</p>`);
-            $(".dayZero").html(`<p>${minD} - ${maxD}`);
-            $(".dayZero").append(`<p>${day}</p>`);
+            $(".dayZero").html(`<p>${day}</p>`);
+            $(".dayZero").append(`<p>${minD} - ${maxD}`);
+            $(".dayZero").append(`<p>${description}</p>`);
 
         }).catch((err) => {
             console.log(err);
         });
 
+
+    }
+
+    function setForecast() {
+
+        $.get("/api/forecast").then(function (data) {
+
+            const dayOneKMin = data.list[2]["main"]["temp"];
+            const dayOneDmin = (dayOneKMin * (9 / 5) - 459.67).toFixed(0);
+            const dayOneKMax = data.list[6]["main"]["temp"];
+            const dayOneDMax = (dayOneKMax * (9 / 5) - 459.67).toFixed(0);
+            const dayOneDesc = data.list[2]["weather"][0]["description"];
+
+            const dayTwoKMin = data.list[10]["main"]["temp"];
+            const dayTwoDMin = (dayTwoKMin * (9 / 5) - 459.67).toFixed(0);
+            const dayTwoKMax = data.list[14]["main"]["temp"];
+            const dayTwoDMax = (dayTwoKMax * (9 / 5) - 459.67).toFixed(0);
+            const dayTwoDesc = data.list[10]["weather"][0]["description"];
+
+            const dayThreeKMin = data.list[19]["main"]["temp"];
+            const dayThreeDmin = (dayThreeKMin * (9 / 5) - 459.67).toFixed(0);
+            const dayThreeKMax = data.list[23]["main"]["temp"];
+            const dayThreeDMax = (dayThreeKMax * (9 / 5) - 459.67).toFixed(0);
+            const dayThreeDesc = data.list[19]["weather"][0]["description"];
+
+            const dayFourKMin = data.list[27]["main"]["temp"];
+            const dayFourDmin = (dayFourKMin * (9 / 5) - 459.67).toFixed(0);
+            const dayFourKMax = data.list[31]["main"]["temp"];
+            const dayFourDMax = (dayFourKMax * (9 / 5) - 459.67).toFixed(0);
+            const dayFourDesc = data.list[27]["weather"][0]["description"];
+
+            let dayZero;
+            let dayOne;
+            let dayTwo;
+            let dayThree;
+            let dayFour;
+
+            switch (new Date().getDay()) {
+                case 0:
+                    dayZero = "Sunday";
+                    dayOne = "Monday";
+                    dayTwo = "Tuesday";
+                    dayThree = "Wednesday";
+                    dayFour = "Thursday";
+                    break;
+                case 1:
+                    dayZero = "Monday";
+                    dayOne = "Tuesday";
+                    dayTwo = "Wednesday";
+                    dayThree = "Thursday";
+                    dayFour = "Friday";
+                    break;
+                case 2:
+                    dayZero = "Tuesday";
+                    dayOne = "Wednesday";
+                    dayTwo = "Thursday";
+                    dayThree = "Friday";
+                    dayFour = "Saturday";
+                    break;
+                case 3:
+                    dayZero = "Wednesday";
+                    dayOne = "Thursday";
+                    dayTwo = "Friday";
+                    dayThree = "Saturday";
+                    dayFour = "TSunday";
+                    break;
+                case 4:
+                    dayZero = "Thursday";
+                    dayOne = "Friday";
+                    dayTwo = "Saturday";
+                    dayThree = "Sunday";
+                    dayFour = "Monday";
+                    break;
+                case 5:
+                    dayZero = "Friday";
+                    dayOne = "Saturday";
+                    dayTwo = "Sunday";
+                    dayThree = "Monday";
+                    dayFour = "Tuesday";
+                    break;
+                case 6:
+                    dayZero = "Saturday";
+                    dayOne = "Sunday";
+                    dayTwo = "Monday";
+                    dayThree = "Tuesday";
+                    dayFour = "Wednesday";
+            }
+
+            const one = $(".dayOne");
+            const two = $(".dayTwo")
+            const three = $(".dayThree");
+            const four = $(".dayFour");
+
+            one.html(`<p>${dayOne}`);
+            one.append(`<p>${dayOneDmin} - ${dayOneDMax}`);
+            one.append(`<p>${dayOneDesc}`);
+
+            two.html(`<p>${dayTwo}`);
+            two.append(`<p>${dayTwoDMin} - ${dayTwoDMax}`);
+            two.append(`<p>${dayTwoDesc}`);
+
+            three.html(`<p>${dayThree}`);
+            three.append(`<p>${dayThreeDmin} - ${dayThreeDMax}`);
+            three.append(`<p>${dayThreeDesc}`);
+
+            four.html(`<p>${dayFour}`);
+            four.append(`<p>${dayFourDmin} - ${dayFourDMax}`);
+            four.append(`<p>${dayFourDesc}`);
+
+        });
 
     }
 
@@ -109,5 +218,6 @@ $(document).ready(function () {
     //handles motion of clock hands every second.
     setInterval(setDate, 1000);
     setTemp();
+    setForecast();
     setInterval(progressBar, 6000);
 });
